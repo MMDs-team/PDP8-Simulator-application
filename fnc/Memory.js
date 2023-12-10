b8684c1b0bd2e9be3442b9ed5c7c90ee61cf1f74
mohammad ghaderi <mohammadunique2002@gmail.com>
2023-12-10 22:03:08 +0330

    start sending the file value to text box

--- fnc/Memory.js
+++ fnc/Memory.js
@@ -1,4 +1,20 @@
 const Word = require('./Word.js');
+const PDP = require('./PDP.js');
+
+
+const textBox = document.getElementById("text-box")
+const File = document.getElementById("file-inp")
+const sendFile = document.getElementById("send-file")
+
+sendFile.addEventListener("click", () => {
+    let text = File.value
+    text = text.split('\n')
+
+    text.forEach(t => {
+        console.log(t)
+        console.log('hello')
+    });
+})
 
 
 class Memory extends Word{
@@ -6,3 +22,5 @@ class Memory extends Word{
     
 
 }
+
+
