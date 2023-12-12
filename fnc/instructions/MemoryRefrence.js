86775a4d2922236f368d65c7078ee2bbdeb685a8
mmd <mohmad1382m@gmail.com>
2023-12-12 20:40:55 +0330

    updated and added memory refrence instructions

--- fnc/instructions/MemoryRefrence.js
+++ fnc/instructions/MemoryRefrence.js
@@ -10,31 +10,53 @@ class MemoryRefrecne extends Instruction{
     AR = 0
     DR = 0
 
-    constructor( AR, DR) {
+    constructor(AR, DR) {
         super()
         this.AR = AR
         this.DR = DR
     }
 
-
     AND(ref) {
-        const getMemVal = PDP.getMem(ref) 
+        const memVal = PDP.getMem(ref) 
         const ACValue = AC.getMem() 
-        const answer = ACValue & getMemVal 
+        const answer = ACValue & memVal 
         AC.setMem(answer)
     }
 
-    ADD(ref) {}
+    ADD(ref) {
+        const memVal = PDP.getMem(ref) 
+        const ACValue = AC.getMem() 
+        const answer = ACValue + memVal 
+        AC.setMem(answer)
+    }
 
-    LDA(ref) {}
+    LDA(ref) {
+        const memVal = PDP.getMem(ref) 
+        AC.setMem(memVal)
+    }
 
-    STA(ref) {}
+    STA(ref) {
+        const ACValue = AC.getMem() 
+        PDP.setMem(ref , getMemVal)
+    }
 
-    BUN(ref) {}
+    BUN(ref) {
+        const memVal = PDP.getMem(ref) 
+        ProgramCounter.load(memVal) 
+    }
 
-    BSA(ref) {}
+    BSA(ref) {
+        const ACValue = AC.getMem()
+        PDP.setMem(ref , ACValue)
+        ref += 1 
+        AC.setMem(ref)
+    }
 
-    ISZ(ref) {}
+    ISZ(ref) {
+        DR = PDP.getMem(ref) + 1 
+        PDP.setMem(ref , DR) 
+        if(DR == 0) ProgramCounter.increment() 
+    }
 }
 
 module.exports = MemoryRefrecne;
