8b9aa30015bf5886a542d43c52d7a6144c6e5649
Mohammad Khandani <mdKhandan28@gmail.com>
2023-12-17 15:56:43 +0330

    adding the bodies of ArithmeticLogical class's methods

--- fnc/instructions/ArithmeticLogical.js
+++ fnc/instructions/ArithmeticLogical.js
@@ -1,15 +1,12 @@
-const { AC, E } = require('../registers/AC');
+const { AC, E } = require('../registers/AC')
 const ProgramConter = require('../ProgramConter')
+const { Control } = require('../PDP.js')
 
 class ArithmeticLogical extends Instruction {
 
-    CLA() {
-        AC.setMem(0);
-    }
+    CLA() { AC.setMem(0); }
 
-    CLE() {
-        E.setMem(false);
-    }
+    CLE() { E.setMem(false); }
 
     CMA() {
         const currVal = AC.getMem()
@@ -19,26 +16,43 @@ class ArithmeticLogical extends Instruction {
     CME() { E.setMem(!E.getMem()) }
 
     CIR() {
-        const eVal = E.getMem();
-        E.setMem(AC.getMem() && 1);
+        const eVal = Number(E.getMem())
+        E.setMem(Boolean(AC.getMem() & 1))
 
-        const toAssign = (AC.getMem() >> 1) & (eVal << 16);
-        AC.setMem(toAssign);
+        const toAssign = (AC.getMem() >> 1) | (eVal << (AC.size() - 1 /* Index base: 0 */))
+        AC.setMem(toAssign)
     }
 
     CIL() {
+        const eVal = Number(E.getMem())
+        E.setMem(Boolean(AC.getMem() & (1 << (AC.size() - 1 /* Index base: 0 */))))
 
+        const toAssign = ((AC.getMem() << 1) | eVal)
+        AC.setMem(toAssign)
     }
 
-    INC() {}
+    INC() {
+        const toAssign = AC.getMem() + 1;
+        AC.setMem(toAssign)
+        E.setMem(Boolean(toAssign & (1 << AC.size() /* Index base: 0 */)))
+    }
 
-    SPA() {}
+    SPA() {
+        // In this instruction we include zero to positive values too!
+        if (!(Boolean(AC.getMem() & (1 << (AC.size() - 1))))) ProgramCounter.increment()
+    }
 
-    SNA() {}
+    SNA() {
+        if (Boolean(AC.getMem() & (1 << (AC.size() - 1)))) ProgramConter.increment()
+    }
 
-    SZA() {}
+    SZA() {
+        if (AC.getMem() == 0) ProgramCounter.increment()
+    }
 
-    SZE() {}
+    SZE() {
+        if (!E.getMem()) ProgramCounter.increment()
+    }
 
-    HLT() {}
+    HLT() { Control.isOn = false; }
 }
