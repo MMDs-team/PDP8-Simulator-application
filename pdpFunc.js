b5932648ab435d8b300715822f5974212a26a348
mmd <mohmad1382m@gmail.com>
2023-12-23 23:49:09 +0330

    update loadAdd and deposite in pdpFunc

--- pdpFunc.js
+++ pdpFunc.js
@@ -1,8 +1,28 @@
+const { ProgramConter } = require('fnc/ProgramConter.js')
+const PDP = require('fnc/PDP.js')
+
 const start = () => {}
 
-const loadAdd = () => {}
+const loadAdd = (swch) => {
+    var value = 0 
+    var mul = 1 
+    for(var index = 17 ; index >= 6 ; index--){
+        value += swch[index] * mul 
+        mul *= 2 
+    }
+    ProgramConter.load(value)
+}
 
-const deposit = () => {}
+const deposit = (swch) => {
+    var address = PDP.getAR() 
+    var value = 0 
+    var mul = 1 
+    for(var index = 17 ; index >= 2 ; index--){
+        value += swch[index] * mul 
+        mul *= 2 
+    }
+    PDP.setMem(address , value) 
+}
 
 const examinate = () => {}
 
