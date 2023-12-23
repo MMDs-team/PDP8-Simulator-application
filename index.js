daed067c4ee2158a95f52302034dbd815d19c044
mohammad ghaderi <mohammadunique2002@gmail.com>
2023-12-23 13:44:37 +0330

    the place of function rearranged after each control swich call , the lights and registers box updated

--- index.js
+++ index.js
@@ -3,131 +3,9 @@
 const labelItems = document.querySelectorAll(".label--item")
 const items = document.querySelectorAll(".left--part")
 
-labelItems[0].addEventListener("click", () => {
-    if (labelItems[0].classList.contains("active")) return
-    labelItems[0].classList.add("active")
-    labelItems[1].classList.remove("active")
-    items[0].classList.add("active")
-    items[1].classList.remove("active")
-})
-
-labelItems[1].addEventListener("click", () => {
-    if (labelItems[1].classList.contains("active")) return
-    labelItems[1].classList.add("active")
-    labelItems[0].classList.remove("active")
-    items[1].classList.add("active")
-    items[0].classList.remove("active")
-})
-
-// ////////////example/////////////////////
-// const x = document.getElementById("right--pdp")
-// x.textContent = window.api.sayHello()
-// document.getElementById("right--pdp").textContent = window.api.sayHello()
-// document.getElementById("right--pdp").textContent = window.api.sayHi()
-// console.log(window.api)
-////////////////////////////////
-
-
 const textArea = document.getElementById("exampleTextarea")
 const sendBtn = document.getElementById("sendAssem")
 let assembelyText = []
-
-const validateAssembly = () => {
-    assembelyText.forEach((inst, i) => {
-        let instruction = inst.split(' ')
-        try {
-            window.api.validateInstruction(inst)
-        } catch (error) {
-            // alert and return
-            return false
-        }
-    })
-    return true
-}
-
-
-sendBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-
-    assembelyText = textArea.value.splite('\n')
-    if (validateAssembly()) {
-        window.api.addToMemory(assembelyText)
-    }
-})
-
-
-
-///////////////////////// PDP-8 //////////////////////////
-
-const switchesVal = document.querySelectorAll(".switch-vl")
-const switchesCon = document.querySelectorAll(".switch-control")
-
-
-let valueSwitches = [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0]
-let controlSwitches = [0,0,0,0, 0,0,0,0]
-
-
-const checkFunc = (switchNumber) => {
-    switch (switchNumber) {
-        case 0: //start
-            window.api.start()
-            break;
-        case 1: // load address
-            window.api.loadAdd(valueSwitches)
-            break;
-        case 2: // deposit
-            window.api.deposit(valueSwitches)
-            break;
-        case 3: // examinate
-            window.api.examinate()
-            break;
-        case 4: // continue
-            window.api.continueSw()
-            break;
-        case 5: // stop
-            window.api.stop()
-            break;
-        case 6: // sing step
-            window.api.singStep()
-            break;
-        case 7: // sing inst
-            window.api.singInst()
-            break;
-        default: 
-            break;
-    }
-}
-
-switchesVal.forEach((swch, i) => {
-    swch.addEventListener("click", (e) => {
-        if (valueSwitches[i] === 0) {
-            valueSwitches[i] = 1;
-            swch.classList.add("active")
-        } else {
-            valueSwitches[i] = 0
-            swch.classList.remove("active")
-        }
-    })
-})
-
-
-
-switchesCon.forEach((swch, i) => {
-    swch.addEventListener("click", (e) => {
-        if (controlSwitches[i] === 0) {
-            controlSwitches[i] = 1;
-            swch.classList.add("active")
-        } else {
-            controlSwitches[i] = 0
-            swch.classList.remove("active")
-        }
-    })
-})
-
-
-
-// ////////////////////////////////////////
-
 
 const acNibbles = document.querySelectorAll(".ac--box .nibble-box")
 const pcNibbles = document.querySelectorAll(".pc--box .nibble-box")
@@ -140,6 +18,13 @@ const PCLights = document.querySelectorAll(".pc-lights span")
 const ARLights = document.querySelectorAll(".ar-lights span")
 const DRLights = document.querySelectorAll(".dr-lights span")
 const ACLights = document.querySelectorAll(".ac-lights span")
+
+const switchesVal = document.querySelectorAll(".switch-vl")
+const switchesCon = document.querySelectorAll(".switch-control")
+
+let valueSwitches = [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0,0]
+let controlSwitches = [0,0,0,0, 0,0,0,0]
+
 
 
 const updateLights = (registersValues) => {
@@ -201,5 +86,107 @@ const updateRegistersBox = () => {
 
     updateLights(registersValues)
 }
+
+
+
+labelItems[0].addEventListener("click", () => {
+    if (labelItems[0].classList.contains("active")) return
+    labelItems[0].classList.add("active")
+    labelItems[1].classList.remove("active")
+    items[0].classList.add("active")
+    items[1].classList.remove("active")
+})
+
+labelItems[1].addEventListener("click", () => {
+    if (labelItems[1].classList.contains("active")) return
+    labelItems[1].classList.add("active")
+    labelItems[0].classList.remove("active")
+    items[1].classList.add("active")
+    items[0].classList.remove("active")
+})
+
+
+
+const validateAssembly = () => {
+    assembelyText.forEach((inst, i) => {
+        let instruction = inst.split(' ')
+        try {
+            window.api.validateInstruction(inst)
+        } catch (error) {
+            // alert and return
+            return false
+        }
+    })
+    return true
+}
+
+
+sendBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    assembelyText = textArea.value.splite('\n')
+    if (validateAssembly()) {
+        window.api.addToMemory(assembelyText)
+    }
+})
+
+const checkFunc = (switchNumber) => {
+    switch (switchNumber) {
+        case 0: //start
+            window.api.start()
+            break;
+        case 1: // load address
+            window.api.loadAdd(valueSwitches)
+            break;
+        case 2: // deposit
+            window.api.deposit(valueSwitches)
+            break;
+        case 3: // examinate
+            window.api.examinate()
+            break;
+        case 4: // continue
+            window.api.continueSw()
+            break;
+        case 5: // stop
+            window.api.stop()
+            break;
+        case 6: // sing step
+            window.api.singStep()
+            break;
+        case 7: // sing inst
+            window.api.singInst()
+            break;
+        default: 
+            break;
+    }
+    updateRegistersBox()
+}
+
+switchesVal.forEach((swch, i) => {
+    swch.addEventListener("click", (e) => {
+        if (valueSwitches[i] === 0) {
+            valueSwitches[i] = 1;
+            swch.classList.add("active")
+        } else {
+            valueSwitches[i] = 0
+            swch.classList.remove("active")
+        }
+    })
+})
+
+
+
+switchesCon.forEach((swch, i) => {
+    swch.addEventListener("click", (e) => {
+        if (controlSwitches[i] === 0) {
+            controlSwitches[i] = 1;
+            swch.classList.add("active")
+        } else {
+            controlSwitches[i] = 0
+            swch.classList.remove("active")
+        }
+    })
+})
+
 
 updateRegistersBox()
