115e977961273d50c756e26c693e1256dd6c1440
mohammad ghaderi <mohammadunique2002@gmail.com>
2023-12-28 17:02:51 +0330

    the memory panel added

--- fnc/Memory.js
+++ fnc/Memory.js
@@ -3,9 +3,8 @@ const PDP = require('./PDP.js')
 
 
 class Memory {
-    
-    static addToMemory(instructions) {
 
+    static findValue(inst) {
         const instOpcode = {
             'AND': [ '0', '8' ],
             'ADD': [ '1', '9' ],
@@ -41,26 +40,46 @@ class Memory {
             'reg': [ 'CLA', 'CLE', 'CMA', 'CME', 'CIR', 'CIL', 'INC', 'SPA', 'SNA', 'SZA', 'SZE', 'HLT'],
             'iot': [ 'INP', 'OUT', 'SKI', 'SKO', 'ION', 'IOF']
         }
+        
+        let instruction = inst.split(' ')
+        // convert a 4 carachter address to a decimal
+        const address = parseInt(instruction[0].slice(0,-1), 16)
+        let value = ''
+        // if it is memeory reference
+        if (instructName['mem'].includes(instruction[1])) {
+            if (instruction[3]==='I')
+                value = instOpcode[instruction[1]][1] + instruction[2]
+            else
+                value = instOpcode[instruction[1]][0] + instruction[2]
+        } else if (instructName['reg'].includes(instruction[1])) { // if it is register reference
+            value = instOpcode[instruction[1]]
+        } else if (instructName['iot'].includes[instruction[1]]) { // if it is input output reference
+            value = instOpcode[instruction[1]]
+        } else {// if it is data
+            value = instruction[1]
+        } 
+        const answer = [address, parseInt(value, 16), value]
+        return answer
+    }
+
     
+    static getBinHex(instructions) {
+        let answer = new Array(instructions.length)
         instructions.forEach((inst, i) => {
-            let instruction = inst.split(' ')
-            // convert a 4 carachter address to a decimal
-            const address = parseInt(instruction[0].slice(0,-1), 16)
-            let value = ''
-            // if it is memeory reference
-            if (instructName['mem'].includes(instruction[1])) {
-                if (instruction[3]==='I')
-                    value = instOpcode[instruction[1]][1] + instruction[2]
-                else
-                    value = instOpcode[instruction[1]][0] + instruction[2]
-            } else if (instructName['reg'].includes(instruction[1])) { // if it is register reference
-                value = instOpcode[instruction[1]]
-            } else if (instructName['iot'].includes[instruction[1]]) { // if it is input output reference
-                value = instOpcode[instruction[1]]
-            } else {// if it is data
-                value = instruction[1]
-            } 
-            const amount = parseInt(value, 16)
+            const [address, amount, value] = this.findValue(inst)
+            answer[i] = {
+                'dec': amount, 
+                'bin': '0'.repeat(16 - amount.toString(2).slice(-16).length) + amount.toString(2).slice(-16),
+                'hex': value,
+                'address': address,
+            }
+        })
+        return answer
+    }
+    
+    static addToMemory(instructions) {        
+        instructions.forEach((inst, i) => {
+            const [address, amount, value] = this.findValue(inst)
             
             PDP.PDP.setMem(address, amount)
         })
@@ -70,7 +89,7 @@ class Memory {
 
 module.exports = {
     Memory, 
-    addToMemory: Memory.addToMemory, 
-    sayHi: Memory.sayHi
+    addToMemory: Memory.addToMemory,
+    getBinHex: Memory.getBinHex,
 }
 
