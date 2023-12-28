dc0c7ae243f249d4870302c96531a195b077c573
Mohammad Khandani <mdKhandan28@gmail.com>
2023-12-28 18:57:32 +0330

    Function findWord added to Word.js
    findWord is function that gets an arg called
    address and returns the state of word in that address of Memory in PDP as output

--- fnc/Word.js
+++ fnc/Word.js
@@ -1,4 +1,5 @@
-
+const PDP = require("./PDP")
+const ProgramCounter = require("./ProgramCounter")
 
 class Word{
     #mem = 0;
@@ -7,8 +8,67 @@ class Word{
         this.#mem = newMem
     }
 
-    getMem() { return this.#mem;}
-    
+    getMem() { return this.#mem; }
 }
 
-module.exports = Word;
+const _instIdentify = (addr, toCon) => {
+    result = [addr.toString(16).toUpperCase().slice(-3)/*, inst, blank/targetAddr, blank/I*/]
+    result[0] = '0'.repeat(3 - result[0].length) + result[0]
+
+    let I = Boolean(toCon & (1 << 15))
+    let d = (toCon & ~(1 << 15)) >> 12
+    let address = toCon & ((1 << 12) - 1)
+
+    const memRefInsts = ["AND", "ADD", "LDA", "STA", "BUN", "BSA", "ISZ"]
+    const registerInsts = ["CLA", "CLE", "CMA", "CME", "CIR", "CIL", "INC", "SPA", "SNA", "SZA", "SZE", "HLT"]
+    const iO = ["INP", "OUT", "SKI", "SKO", "ION", "IOF"]
+
+    if (I && d !== 7) address = PDP.getMem(address)
+
+    switch (d) {
+        case 7:
+            // Identify the data elems from counting pop bits of the adress and then control the flow!
+            let popCnt = 0
+            let cmdFlag = -1
+            for (let i = 0; i < 12; i++) {
+                if (Boolean(address & (1 << i))) {
+                    cmdFlag = 12 - i - 1 /* Because of index basing equal to ZERO */;
+                    popCnt++
+                }
+            }
+
+            if (popCnt != 1) return []
+
+            if (!I) result.push(registerInsts[cmdFlag])
+            else result.push(iO[cmdFlag])
+
+            break;
+
+        default:
+            address = adress.toString(16).toUpperCase().slice(-3)
+            address = '0'.repeat(3 - address.length) + adress
+
+            result.push(memRefInsts[d], address)
+            if (I) result.push('I')
+    }
+
+    return result
+}
+
+const findWord = (address) => {
+    const arbWord = PDP.PDP.getMem(address)
+
+    let answer = {
+        'data': parseInt(arbWord, 16).toUpperCase().slice(-4),
+        'instruction': _instIdentify(address, arbWord),
+        'isCurrent': ProgramCounter.getMem() === address,
+        'address': address.toString(16).toUpperCase().slice(-3)
+    }
+
+    answer.data = '0'.repeat(4 - answer.data.length) + answer.data
+    answer.address = '0'.repeat(3 - answer.addresss.length) + answer.address
+    
+    return answer
+}
+
+module.exports = { Word, findWord };
