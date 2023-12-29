b0e7115d118f4de016b3dcba2a3daac5fd00fe07
mmd <mohmad1382m@gmail.com>
2023-12-29 17:19:25 +0330

    add createCodeLine & validateCodeLine  function

--- fnc/instructions/Instruction.js
+++ fnc/instructions/Instruction.js
@@ -1,7 +1,5 @@
 class Instruction {
 
-    static validateInstruction(inst) {
-
         // return value status code :
         // 100 : valid instruction 
         // 101 : wrong first address 
@@ -11,6 +9,8 @@ class Instruction {
         // 105 : wrong data char 
         // 106 : data length not enough
         // 107 : inst length not enough 
+
+    static validateInstruction(inst) {
 
         const instructName = {
             'mem': [ 'AND', 'ADD', 'LDA', 'STA', 'BUN', 'BSA', 'ISZ'],
@@ -64,6 +64,114 @@ class Instruction {
             default :
                 // it is wrong!!!!
                 return 107
+        }
+    }
+
+    static createCodeLine(inst){
+        let codeLine = []
+        let labels = {}
+        let instLen = inst.length
+        let lineNumber = parseInt(inst[0][1] , 16) ;
+        let validateRes = 0 
+        let lineLen = 0
+        let txtLen = 0
+        let hexaNum = 0
+
+        let pushArr = []
+        let pushAdd = 0
+
+        for(let index = 1 ; index < instLen ; index++){
+            validateRes = this.validateCodeLine(inst[index])
+            if(validateRes != 100) return validateRes 
+            
+            txtLen = inst[index][0].length 
+            if(inst[index][0][txtLen - 1] == ','){
+                hexaNum = lineNumber.toString(16) ; 
+                if(hexaNum.length == 1) hexaNum = "00" + hexaNum
+                else if (hexaNum.length == 2) hexaNum = "0" + hexaNum
+                labels[inst[index][0].slice(0 , txtLen - 1)] = hexaNum.toUpperCase() ;
+            }
+            lineNumber += 1 
+        }
+
+        lineNumber = parseInt(inst[0][1] , 16) ;
+
+        for(let index = 1 ; index < instLen ; index++){
+            lineLen = inst[index].length 
+            pushArr = []
+
+            pushAdd = lineNumber.toString(16) ;
+            if(pushAdd.length == 1) pushAdd = "00" + pushAdd + ":"
+            else if(pushAdd.length == 2) pushAdd = "0" + pushAdd + ":"
+            else pushAdd = pushAdd + ":"
+            pushArr.push(pushAdd.toUpperCase())
+
+            
+            switch(lineLen){
+                case 1:
+                    pushArr.push(inst[index][0])
+                    break ; 
+                case 2:
+                    if(inst[index][0][inst[index][0].length - 1] == ','){
+                        pushArr.push(inst[index][1])
+                    }else{
+                        pushArr.push(inst[index][0])
+                        pushArr.push(labels[inst[index][1]])
+                    }
+                    break ;
+                case 3:
+                    pushArr.push(inst[index][0])
+                    pushArr.push(labels[inst[index][1]])
+                    pushArr.push(inst[index][2])
+                    break ;
+                }
+            codeLine.push(pushArr)
+            lineNumber += 1 
+        }
+        return codeLine
+    }
+
+
+    static validateCodeLine(inst){
+        
+        const instructName = {
+            'mem': [ 'AND', 'ADD', 'LDA', 'STA', 'BUN', 'BSA', 'ISZ'],
+            'reg': [ 'CLA', 'CLE', 'CMA', 'CME', 'CIR', 'CIL', 'INC', 'SPA', 'SNA', 'SZA', 'SZE', 'HLT'],
+            'iot': [ 'INP', 'OUT', 'SKI', 'SKO', 'ION', 'IOF'],
+            'needLabel' : []
+        }
+    
+        const hex = '0123456789ABCDEF'
+
+        switch (inst.length) {
+            case 1:
+                // this line only can be a register or I/O instruction 
+
+                if(instructName["reg"].includes(inst[0]) || instructName["iot"].includes(inst[0])) return 100
+                return 103
+            case 2:
+                // label, data 
+                if(inst[0].length >= 1 && inst[0][inst[0].length - 1] == ','){
+                    // check data validation of data  
+                    if(inst[1].length !== 4) return 106 
+                    for(let index = 0 ; index < 4 ; index++) if(!hex.includes(inst[1][index])) return 105
+                    return 100
+                }
+                // inst label (this line only can be a memory instruction)
+                else if(instructName["mem"].includes(inst[0])) return 100 
+
+                return 103              
+            case 3:
+                // inst label I (this line only can be a memory instruction)
+                if(!instructName["mem"].includes(inst[0])) return 103 
+
+                // check indirect char 
+                if(inst[2] != "I") return 104 
+
+                return 100
+            default :
+                // length not enough or to long 
+                return 107 
         }
     }
 }
