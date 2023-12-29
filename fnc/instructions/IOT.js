5f88a2d02f12b624266109a1d43b49eea759060c
Mohammad Khandani <mdKhandan28@gmail.com>
2023-12-29 22:46:41 +0330

    IOT instructions body have been added!

--- fnc/instructions/IOT.js
+++ fnc/instructions/IOT.js
@@ -1,12 +1,38 @@
+const { AC, INPR, IF, OUTR, OF } = require("../Register.js")
+const ProgramCounter = require("../ProgramCounter.js")
+
 
 class IOT {
 
-    static INP() {}
-    static OUT() {}
-    static SKI() {}
-    static SKO() {}
-    static ION() {}
-    static IOF() {}
+    static INP() {
+        if (IF.getMem()) {
+            const acVal = AC.getMem()
+            const inpVal = INPR.getMem()
+
+            const toAssign = ((acVal >> 8) << 8) + inpVal
+            AC.setMem(toAssign)
+            IF.setMem(false)
+        }
+    }
+    static OUT() {
+        if (OF.getMem()) {
+            const toAssign = AC.getMem() & ((1 << 8) - 1)
+
+            OUTR.setMem(toAssign)
+            OF.setMem(false)
+        }
+    }
+    static SKI() {
+        if (IF.getMem()) ProgramCounter.load(ProgramCounter.get() + 1)
+    }
+    static SKO() {
+        if (OF.getMem()) ProgramCounter.load(ProgramCounter.get() + 1)
+    }
+    static ION() { IEN.setMem(true) }
+    static IOF() { IEN.setMem(false) }
+
+    static assignToINPR(data) {
+    }
 }
 
 module.exports = IOT;
